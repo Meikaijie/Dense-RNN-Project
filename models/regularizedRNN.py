e1ab121293641912a3ef56edec11c92ca52ed7ea
Sahil Kanjiyani <sahilkanjiyani@Sahils-MacBook-Pro.local>
2017-11-18 13:31:40 -0500

    regualurizedRNN

--- models/regularizedRNN.py
+++ models/regularizedRNN.py
@@ -5,6 +5,17 @@ import itertools
 import numpy as np
 import matplotlib.pyplot as plt
 
+num_epochs = 100
+total_series_length = 50000 * 100
+regularizers = [2, 4, 8]
+truncated_backprop_length = regularizers[-1] * 2
+state_size = 4
+num_classes = 2
+layers = 2
+echo_step = 3
+batch_size = 5
+num_batches = total_series_length//batch_size//truncated_backprop_length
+
 def dRNN(cell, inputs, rate, scope='default'):
     """
     This function constructs a layer of dilated RNN.
@@ -133,6 +144,7 @@ def _rnn_reformat(x, input_dims, n_steps):
 def drnn_classification(x,
                         hidden_structs,
                         dilations,
+                        regularizers,
                         n_steps,
                         n_classes,
                         input_dims=1,
@@ -143,6 +155,7 @@ def drnn_classification(x,
         x -- a tensor of shape (batch_size, n_steps, input_dims).
         hidden_structs -- a list, each element indicates the hidden node dimension of each layer.
         dilations -- a list, each element indicates the dilation of each layer.
+        regularizers -- a list, indicating which connections back you should use to make predictions
         n_steps -- the length of the sequence.
         n_classes -- the number of classes for the classification.
         input_dims -- the input dimension.
@@ -152,6 +165,7 @@ def drnn_classification(x,
         pred -- the prediction logits at the last timestamp and the last layer of the RNN.
                 'pred' does not pass any output activation functions.
     """
+    global batch_size
     # error checking
     assert (len(hidden_structs) == len(dilations))
 
@@ -160,38 +174,29 @@ def drnn_classification(x,
 
     # construct a list of cells
     cells = _contruct_cells(hidden_structs, cell_type)
-    cells2 = _contruct_cells(hidden_structs, cell_type)
 
     # define dRNN structures
     with tf.variable_scope('forward'):
         layer_outputs = multi_dRNN_with_dilations(cells, x_reformat, dilations)
-    x_reformat.reverse()
-    with tf.variable_scope('backward'):
-        layer_outputs_backwards = multi_dRNN_with_dilations(cells2, x_reformat, dilations)
-    layer_outputs_backwards.reverse()
-    final_outputs = [tf.concat([layer_outputs[i], layer_outputs_backwards[i]], 1) for i in range(n_steps)]
 
     if dilations[0] == 1:
         # dilation starts at 1, no data dependency lost
         # define the output layer
-        weights = tf.Variable(tf.random_normal(shape=[hidden_structs[-1] * 2,
+        weights = tf.Variable(tf.random_normal(shape=[hidden_structs[-1] * (len(regularizers) + 1),
                                                       n_classes]))
         bias = tf.Variable(tf.random_normal(shape=[n_classes]))
-        # define prediction
-        preds = [tf.matmul(state, weights) + bias for state in final_outputs]
+
+        preds = []
+        for i in range(len(layer_outputs)):
+            to_concat = [layer_outputs[i]] 
+            for j in regularizers:
+                if i-j >= 0:
+                    to_concat.append(layer_outputs[i-j])
+                else:
+                    to_concat.append(np.zeros((batch_size, hidden_structs[-1])))
+            preds.append(tf.matmul(tf.concat(to_concat, 1), weights) + bias)
 
     return preds
-
-num_epochs = 100
-total_series_length = 50000 * 100
-truncated_backprop_length = 15
-state_size = 4
-num_classes = 2
-layers = 2
-echo_step = 3
-batch_size = 5
-num_batches = total_series_length//batch_size//truncated_backprop_length
-dilations = [1, 2, 4, 8, 16, 32, 64, 128, 256]
 
 def generateData():
     x = np.array(np.random.choice(2, total_series_length, p=[0.9, 0.1]))
@@ -207,10 +212,12 @@ batchX_placeholder = tf.placeholder(tf.float32, [batch_size, truncated_backprop_
 batchY_placeholder = tf.placeholder(tf.int32, [batch_size, truncated_backprop_length])
 
 labels_series = tf.unstack(batchY_placeholder, axis=1)
+labels_series = labels_series[truncated_backprop_length//2:]
 
 x_input = tf.expand_dims(batchX_placeholder, 2)
 
-logits_series = drnn_classification(x_input, [state_size] * layers, dilations[0:layers], truncated_backprop_length, num_classes)
+logits_series = drnn_classification(x_input, [state_size], [1], regularizers, truncated_backprop_length, num_classes, 1)
+logits_series = logits_series[truncated_backprop_length//2:]
 predictions_series = [tf.nn.softmax(logits) for logits in logits_series]
 
 losses = [tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels) for logits, labels in zip(logits_series,labels_series)]
